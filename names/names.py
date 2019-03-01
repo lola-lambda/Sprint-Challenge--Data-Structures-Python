72078bda77fbaac94df29bdd016ca90900ba618b
Lola <lolahef@gmail.com>
2019-03-01 13:08:59 -0500

    Added 2s approach & 0.01s approach

--- names/names.py
+++ names/names.py
@@ -10,11 +10,29 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# 9s runtime:
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# 2s runtime:
+# duplicates = [name for name in names_1 if name in names_2]
+
+# 0.01s runtime:
+names_1 = list(set(names_1))
+names_2 = list(set(names_2))
+merged_list = names_1 +names_2
+encountered = {}
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+for name in merged_list:
+    if name in encountered:
+        encountered[name] += 1
+        duplicates.append(name)
+    else:
+        encountered[name] = 1
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
