c697cdf194f22c9a3d213e2f2a63e5c1bfc58a18
Lola <lolahef@gmail.com>
2019-03-01 13:15:26 -0500

    Depth first for each implemented

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -1,11 +1,35 @@
+import copy
+
+class Stack:
+  def __init__(self):
+    self.storage = []
+  
+  def push(self, item):
+    self.storage.append(item)
+
+  def pop(self):
+    self.storage.pop()
+  
+  def peek(self):
+    return self.storage[len(self.storage) - 1]
+
 class BinarySearchTree:
   def __init__(self, value):
     self.value = value
     self.left = None
     self.right = None
 
-  def depth_first_for_each(self, cb):
-    pass    
+  def depth_first_for_each(self, cb, stack=Stack(), finished=True):
+    stack.push(self.value)
+    if self.left: 
+      self.left.depth_first_for_each(cb, stack, False)
+    if self.right: 
+      self.right.depth_first_for_each(cb, stack, False)
+    if finished: 
+      if len(stack.storage) > 1:
+        return [cb(x) for x in stack.storage[1:]] 
+      else:
+        return cb(stack.storage[0])
 
   def breadth_first_for_each(self, cb):
     pass
